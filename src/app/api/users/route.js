2d20fbcb2c82b072739011a5a02aa04aa434acc1
Xavier <xavier.s.viel@gmail.com>
2024-06-06 15:38:07 -0300

    commentaires

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,7 +1,9 @@
+//connexion d'un utilisateur au site avec l'aide de mongoDB
 import {isAdmin} from "@/app/api/auth/[...nextauth]/route";
 import {User} from "@/models/User";
 import mongoose from "mongoose";
 
+//trouver l'utilisateur et l'authentifier
 export async function GET() {
   mongoose.connect(process.env.MONGO_URL);
   if (await isAdmin()) {
