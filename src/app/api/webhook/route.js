2e2629d1205cddd13177162128ace774d1bc51fc
Xavier <xavier.s.viel@gmail.com>
2024-06-06 15:43:32 -0300

    commentaires

--- src/app/api/webhook/route.js
+++ src/app/api/webhook/route.js
@@ -1,11 +1,14 @@
+//fonctions pour compléter une commande
 import {Order} from "@/models/Order";
 
+//utiliser stripe pour le paiement
 const stripe = require('stripe')(process.env.STRIPE_SK);
 
 export async function POST(req) {
   const sig = req.headers.get('stripe-signature');
   let event;
 
+  //pour pouvoir attrapper les erreurs, il faut faire ces étapes et comme ceci on peut savoir si il y a eu une erreur avec stripe
   try {
     const reqBuffer = await req.text();
     const signSecret = process.env.STRIPE_SIGN_SECRET;
@@ -16,6 +19,7 @@ export async function POST(req) {
     return Response.json(e, {status: 400});
   }
 
+  //si le paiement est complété, il y aura un nouveau statut sur la commande et on pourra les envoyer au backend pour qu'elles soient complétées
   if (event.type === 'checkout.session.completed') {
     console.log(event);
     const orderId = event?.data?.object?.metadata?.orderId;
