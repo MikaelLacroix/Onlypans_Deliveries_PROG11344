9940d4e0a469e82f8dade2dc6387489bdbfee5ae
Gabriel Theriault <120581491+gabthe0829@users.noreply.github.com>
2024-06-06 13:46:32 -0300

    more styles

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -6,15 +6,15 @@ export default function Hero() {
     <section className="hero md:mt-4">
       <div className="py-8 md:py-12">
         <h1 className="text-4xl font-semibold">
-          Everything<br />
-          is better<br />
-          with a&nbsp;
+          Making<br />
+          food<br />
+          delivery&nbsp;
           <span className="text-primary">
-            Pizza
+            Sexy
           </span>
         </h1>
         <p className="my-6 text-gray-500 text-sm">
-          Pizza is the missing piece that makes every day complete, a simple yet delicious joy in life
+          Onlypans is making cooking and delivering sexy 
         </p>
         <div className="flex gap-4 text-sm">
           <button className="flex justify-center bg-primary uppercase flex items-center gap-2 text-white px-4 py-2 rounded-full">
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#f13a01',
+        primary: '#00b4ef',
       },
     },
   },
